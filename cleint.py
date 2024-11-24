4c1faedbca09b43eafd812cdbdcc430d9a97636d
Najat_Almalood <almaloodn@gmail.com>
2024-11-24 20:08:23 +0300

    basic menu complated

--- cleint.py
+++ cleint.py
@@ -21,7 +21,8 @@ while n==True:
         print("5 back to the main menu ")
         number_2=int(input(" secound enter the required servise number "))
         if number_2==1:
-           msg=b"banana"
+           key_word=input(" key word to be searched with")
+           msg=key_word
         if number_2==2:
             print('select the required catogry')
             print(" 1 business")
@@ -33,23 +34,22 @@ while n==True:
             catogry_num=int(input(" enter required catogry number"))
             match catogry_num:
                 case 1:
-                    cstogry_msg="business_news"
+                    msg="business_news"
                 case 2:
-                    cstogry_msg="genral_news"
+                    msg="genral_news"
                 case 3:
-                    cstogry_msg="health_news"
+                    msg="health_news"
                 case 4:
-                    cstogry_msg="seince_news"
+                    msg="seince_news"
                 case 5:
-                    cstogry_msg="sport_news"
+                    msg="sport_news"
                 case 6:
-                    cstogry_msg="technology_news"
+                    msg="technology_news"
                 case _:
                     print("not vlaid catogry")
-            cstogry_msg_bytes=bytes(cstogry_msg,"ascii")
-            msg=cstogry_msg_bytes
-            #clinet_sock.send(cstogry_msg_bytes)
-            #msg=b"apple"
+                    continue
+            
+
         if number_2==3:
             print('select the required country')
             print(" 1 au")
@@ -65,33 +65,31 @@ while n==True:
 
             match catogry_num:
                 case 1:
-                    cstogry_msg="au_news"
+                   msg="au_news"
                 case 2:
-                    cstogry_msg="ca_news"
+                    msg="ca_news"
                 case 3:
-                    cstogry_msg="jp_news"
+                    msg="jp_news"
                 case 4:
-                    cstogry_msg="ae_news"
+                    msg="ae_news"
                 case 5:
-                    cstogry_msg="sa_news"
+                    msg="sa_news"
                 case 6:
-                    cstogry_msg="kr_news"
+                    msg="kr_news"
                 case 7:
-                    cstogry_msg="us_news"
+                    msg="us_news"
                 case 8:
-                    cstogry_msg="ma_news"
+                    msg="ma_news"
                 case _:
                     print("not vlaid country")
-            cstogry_msg_bytes=bytes(cstogry_msg,"ascii")
-            msg=cstogry_msg_bytes
-
-      
+                    continue
+           
         if number_2==4:
-           msg=b"all_news"
+           msg="all_news"
         if number_2==5:
-            msg=b"quit"
+            msg="quit"
             continue
-        clinet_sock.send(msg)
+        #clinet_sock.send(msg)
     if number==2:    
         print("List of sources menu")
         print("1 search by catogry ")
@@ -111,22 +109,20 @@ while n==True:
             catogry_num=int(input(" enter required catogry number"))
             match catogry_num:
                 case 1:
-                    cstogry_msg="business_sources"
+                    msg="business_sources"
                 case 2:
-                    cstogry_msg="genral_sources"
+                    msg="genral_sources"
                 case 3:
-                    cstogry_msg="health_sources"
+                    msg="health_sources"
                 case 4:
-                    cstogry_msg="seince_sources"
+                    msg="seince_sources"
                 case 5:
-                    cstogry_msg="sport_sources"
+                    msg="sport_sources"
                 case 6:
-                    cstogry_msg="technology_sources"
+                    msg="technology_sources"
                 case _:
                     print("not vlaid catogry")
-            cstogry_msg_bytes=bytes(cstogry_msg,"ascii")
-            msg=cstogry_msg_bytes
-           
+                    continue
         if number_3==2:
             print('select the required country')
             print(" 1 au")
@@ -142,25 +138,24 @@ while n==True:
 
             match catogry_num:
                 case 1:
-                    cstogry_msg="au_sources"
+                    msg="au_sources"
                 case 2:
-                    cstogry_msg="ca_sources"
+                    msg="ca_sources"
                 case 3:
-                    cstogry_msg="jp_sources"
+                    msg="jp_sources"
                 case 4:
-                    cstogry_msg="ae_sources"
+                    msg="ae_sources"
                 case 5:
-                    cstogry_msg="sa_sources"
+                    msg="sa_sources"
                 case 6:
-                    cstogry_msg="kr_sources"
+                    msg="kr_sources"
                 case 7:
-                    cstogry_msg="us_sources"
+                    msg="us_sources"
                 case 8:
-                    cstogry_msg="ma_sources"
+                    msg="ma_sources"
                 case _:
                     print("not vlaid country")
-            cstogry_msg_bytes=bytes(cstogry_msg,"ascii")
-            msg=cstogry_msg_bytes
+                    continue
            
         if number_3==3:
             print("choose the required language")
@@ -169,30 +164,31 @@ while n==True:
             country_num=int(input(" the choosen language number "))
             match country_num:
                 case 1:
-                    country="ar"
+                    msg="ar"
                 case 2 :
-                     country="en"
+                    msg="en"
                 case _:
                     print("not vlaid language")
-            cstogry_msg_bytes=bytes(country,"ascii")
-            msg=cstogry_msg_bytes
-           
+                    continue
         if number_3==4:
-           msg=b"all_language"
+           msg="all_language"
         if number_3==5:
-            msg=b"quit"
+           
+           
+            
+            
+            msg="quit"
             continue
         
-        clinet_sock.send(msg)
+        
     if number==3:
-        msg=b"quit"
+        msg="quit"
         n=False
-
+    clinet_sock.send(msg.encode("ascii"))
  
 data = clinet_sock.recv(1024)
  
 print( "data recived",data.decode("ascii"))
-
 clinet_sock.close()
 
 
